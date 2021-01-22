46ceafdd74f04f91bdb3ed484b903b22ef3cd8d8
Atik <coc13259@gmail.com>
2021-01-22 19:39:26 +0600

    1

--- RDP.sh
+++ RDP.sh
@@ -3,9 +3,13 @@ printf "Installing RDP Be Patience... " >&2
 {
 sudo useradd -m ALOK
 sudo adduser ALOK sudo
-echo 'ALOK:8426' | sudo chpasswd
+echo 'ALOK:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
+
+wget https://dn3.freedownloadmanager.org/6/latest/freedownloadmanager.deb
+sudo dpkg -i freedownloadmanager.deb
+
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
