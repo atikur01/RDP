7c9c62e3a98b2972e88f211e65c683550d66b4e2
MD. ATIKUR RAHMAN <k6eirv@outlook.com>
2021-02-16 00:50:40 +0600

    1

--- RDP.sh
+++ RDP.sh
@@ -7,6 +7,8 @@ echo 'ALOK:1234' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 
+sudo apt install qbittorrent
+
 wget https://dn3.freedownloadmanager.org/6/latest/freedownloadmanager.deb
 sudo dpkg -i freedownloadmanager.deb
 
